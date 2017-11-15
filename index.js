2cb281d494c71f2a03a98a8dade6086d5f3e9296
Tommy Hu <tomxhu@gmail.com>
2017-11-15 11:52:57 -0500

    handle sourceMessageId

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const CONVERSATION_API_BASE = 'https://api.driftqa.com/v1/conversations'
 const TOKEN = process.env.BOT_API_TOKEN
 const GIPHY_API_KEY = process.env.GIPHY_API_KEY
 
-const MESSAGE_ID_TO_UPDATE_BY_CONVERSATION_ID = {}
-
-const createReponseMessage = ({ orgId, giphyLink, searchParam, conversationId, replace = false}) => {
+const createReponseMessage = ({ orgId, giphyLink, searchParam, conversationId, editedMessageId, replace = false}) => {
   const linkToSend = `<a href="${giphyLink}" rel="nofollow">${searchParam}</a>`
   const message = {
     'orgId': orgId,
@@ -23,7 +21,7 @@ const createReponseMessage = ({ orgId, giphyLink, searchParam, conversationId, r
       'type': 'reply',
       'style': 'primary',
       'reaction': {
-        type: 'remove'
+        type: 'delete'
       }
     }, {
       'label': 'Shuffle',
@@ -32,32 +30,27 @@ const createReponseMessage = ({ orgId, giphyLink, searchParam, conversationId, r
     }, {
       'label': 'Cancel',
       'value': 'cancel',
-      'type': 'action',
-      'reaction': {
-        type: 'noop'
-      }
+      'type': 'action', // switch to noop
     },]
   }
-  const editedMessageId = MESSAGE_ID_TO_UPDATE_BY_CONVERSATION_ID[conversationId]
   return replace ? Object.assign(message, { editedMessageId, editType: 'replace' }) : message
 }
 
-const getGifAndSendMessage = (orgId, conversationId, messageId, searchParam, replace = false) => {
+const getGifAndSendMessage = (orgId, conversationId, messageId, searchParam, editedMessageId, replace = false) => {
   return request.get(`https://api.giphy.com/v1/gifs/search?api_key=${GIPHY_API_KEY}&q=${encodeURIComponent(searchParam)}&limit=25&offset=0&rating=G&lang=en`)
     .end((err, response) => {
-      const randomIndex = Math.floor(Math.random() * 25)
+      console.log(response.body.data.length)
+      const length = response.body.data.length
+      const randomIndex = Math.floor(Math.random() * length)
       const giphyLink = response.body.data[randomIndex].url
       return request.post(CONVERSATION_API_BASE + `/${conversationId}/messages`)
         .set('Content-Type', 'application/json')
         .set(`Authorization`, `bearer ${TOKEN}`)
-        .send(createReponseMessage({ orgId, giphyLink, searchParam, conversationId, replace }))
+        .send(createReponseMessage({ orgId, giphyLink, searchParam, conversationId, editedMessageId, replace }))
         .end((err, response) => {
           if (err) {
             console.log(err)
             return
-          }
-          if (!replace) {
-            MESSAGE_ID_TO_UPDATE_BY_CONVERSATION_ID[conversationId] = response.body.data.id
           }
         })
     })
@@ -69,7 +62,7 @@ const handleMessage = (orgId, data) => {
     const conversationId = data.conversationId
     if (messageBody.startsWith('/giphy')) {
       const searchParam = messageBody.replace('/giphy ', '')
-      return getGifAndSendMessage(orgId, conversationId, conversationId, searchParam)
+      return getGifAndSendMessage(orgId, conversationId, conversationId, searchParam, data.id)
     }
   }
 }
@@ -77,7 +70,8 @@ const handleMessage = (orgId, data) => {
 const handleButton = (orgId, data) => {
   const conversationId = data.conversationId
   const searchParam = data.button.value
-  return getGifAndSendMessage(orgId, conversationId, conversationId, searchParam, true)
+  const editedMessageId = data.sourceMessageId
+  return getGifAndSendMessage(orgId, conversationId, conversationId, searchParam, editedMessageId, true)
 }
 
 app.use(bodyParser.json())
